0d7a81ee8164092ec4285ea9a896e81f8480424b
Ngô Ngọc Vũ <157010268+iuh20047731@users.noreply.github.com>
2024-03-28 15:22:37 +0700

    Update init-db.sql

--- init-db.sql
+++ init-db.sql
@@ -1,12 +1,12 @@
-CREATE TABLE IF NOT EXISTS tblbookIUHX (
+CREATE TABLE IF NOT EXISTS tblbook20047731 (
     id serial PRIMARY KEY,
     title varchar NOT NULL,
     author varchar NOT NULL
 );
 
 INSERT INTO tblbook (title, author) VALUES
-  ('DevOps', 'IUHX'),
-  ('Big Data', 'IUHX'),
-  ('Cloud Deployement', 'IUHX'),
-('Data Analysis', 'IUHX'),
-('Block Chain', 'IUHX');
+  ('DevOps', '20047731'),
+  ('Big Data', '20047731'),
+  ('Cloud Deployement', '20047731'),
+('Data Analysis', '20047731'),
+('Block Chain', '20047731');
